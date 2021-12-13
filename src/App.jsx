1f9537530aa29243210a1951fab01f6506e521e1
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-12-13 16:33:03 +0200

    setting up the client side

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [obj, setObj] = useState({ name: "", age: "", country: "", position: "", wage: "" })
+
+  const handleChange = e => setObj({ ...obj, [e.target.id]: e.target.value })
+
+  const addEmployee = () => {
+    axios.post('http://localhost:5000/create', obj);
+  }
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label htmlFor="name">Name:</label>
+        <input onChange={ handleChange } value={ obj.name } type="text" id="name" />
+        <label htmlFor="age">Age:</label>
+        <input onChange={ handleChange } value={ obj.age } type="number" id="age" />
+        <label htmlFor="country">Country:</label>
+        <input onChange={ handleChange } value={ obj.country } type="text" id="country" />
+        <label htmlFor="position">Position:</label>
+        <input onChange={ handleChange } value={ obj.position } type="text" id="position" />
+        <label htmlFor="wage">Wage:</label>
+        <input onChange={ handleChange } value={ obj.wage } type="number" id="wage" />
+        <button onClick={ addEmployee }>Add employee</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
