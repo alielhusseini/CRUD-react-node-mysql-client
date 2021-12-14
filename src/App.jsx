99b582b3b62a4f0e44821e01ce062c4cb9cce3a2
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-12-14 09:45:11 +0200

    posting deleting getting from db

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
   const [obj, setObj] = useState({ name: "", age: "", country: "", position: "", wage: "" })
+  const [employeeList, setEmployeeList] = useState([])
 
   const handleChange = e => setObj({ ...obj, [e.target.id]: e.target.value })
 
   const addEmployee = () => {
-    axios.post('http://localhost:5000/create', obj);
+    axios.post('http://localhost:5000/create', obj)
+      .then(res => setEmployeeList([...employeeList, obj]))
+    setObj({ name: "", age: "", country: "", position: "", wage: "" })
   }
+
+  const getEmployees = () => {
+    axios.get('http://localhost:5000/employees')
+      .then(res => setEmployeeList(res.data))
+  }
+
+  const deleteEmployee = e => {
+    axios.delete(`http://localhost:5000/employees/${e.target.dataset.id}`)
+    .then(res => console.log(res))
+  }
+  
+  useEffect(() => {
+    axios.get('http://localhost:5000/employees')
+      .then(res => setEmployeeList(res.data))
+  }, [employeeList])
 
   return (
     <div className="App">
@@ -24,6 +42,23 @@ function App() {
         <label htmlFor="wage">Wage(year):</label>
         <input onChange={ handleChange } value={ obj.wage } type="number" id="wage" />
         <button onClick={ addEmployee }>Add employee</button>
+      </div>
+      <div className="underline"></div>
+      <div className="employees">
+        <button onClick={getEmployees}>Show employees</button>
+        { employeeList && employeeList.map(emp => {
+          const { id, name, position, country } = emp;
+          return (
+            <div key={ id } className='container'>
+              <div className='employee'>
+                <h2>{ name }</h2>
+                <h3>{ position }</h3>
+                <h4>{ country }</h4>
+              </div>
+              <button data-id={ id } onClick={ deleteEmployee } className='delete'>Delete</button>
+            </div>
+          )
+        }) }
       </div>
     </div>
   );
